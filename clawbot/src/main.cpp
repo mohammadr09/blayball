3c20819d29050a80454eb71551b9bf1070a2a62b
mohammadr09 <mohammadnyc96@gmail.com>
2026-05-08 11:39:26 -0400

    Progress

--- clawbot/src/main.cpp
+++ clawbot/src/main.cpp
@@ -1,11 +1,10 @@
 #pragma region VEXcode Generated Robot Configuration
 // Make sure all required headers are included.
+#include <math.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
-#include <math.h>
 #include <string.h>
-
 
 #include "vex.h"
 
@@ -14,65 +13,57 @@ using namespace vex;
 // Brain should be defined by default
 brain Brain;
 
-
 // START V5 MACROS
-#define waitUntil(condition)                                                   \
-  do {                                                                         \
-    wait(5, msec);                                                             \
-  } while (!(condition))
+#define waitUntil(condition) \
+    do {                     \
+        wait(5, msec);       \
+    } while (!(condition))
 
-#define repeat(iterations)                                                     \
-  for (int iterator = 0; iterator < iterations; iterator++)
+#define repeat(iterations) \
+    for (int iterator = 0; iterator < iterations; iterator++)
 // END V5 MACROS
-
 
 // Robot configuration code.
 motor ClawMotor = motor(PORT10, ratio18_1, false);
 
 motor ArmMotor = motor(PORT1, ratio18_1, false);
 
-controller Controller1 = controller(primary);
 motor LeftMotor = motor(PORT9, ratio18_1, false);
 
 motor RightMotor = motor(PORT8, ratio18_1, true);
 
+controller Controller1 = controller(primary);
 motor55 MC55_11 = motor55(PORT11, false);
 
-sonar RangeFinderC = sonar(Brain.ThreeWirePort.C);
-pot PotentiometerH = pot(Brain.ThreeWirePort.H);
-line LineTrackerE = line(Brain.ThreeWirePort.E);
+sonar RangeFinderA = sonar(Brain.ThreeWirePort.C);
 
+pot Potentiometer = pot(Brain.ThreeWirePort.H);
+line LineTracker = line(Brain.ThreeWirePort.E);
 
 // generating and setting random seed
-void initializeRandomSeed(){
-  int systemTime = Brain.Timer.systemHighResolution();
-  double batteryCurrent = Brain.Battery.current();
-  double batteryVoltage = Brain.Battery.voltage(voltageUnits::mV);
+void initializeRandomSeed() {
+    int systemTime = Brain.Timer.systemHighResolution();
+    double batteryCurrent = Brain.Battery.current();
+    double batteryVoltage = Brain.Battery.voltage(voltageUnits::mV);
 
-  // Combine these values into a single integer
-  int seed = int(batteryVoltage + batteryCurrent * 100) + systemTime;
+    // Combine these values into a single integer
+    int seed = int(batteryVoltage + batteryCurrent * 100) + systemTime;
 
-  // Set the seed
-  srand(seed);
+    // Set the seed
+    srand(seed);
 }
-
-
 
 void vexcodeInit() {
-
-  //Initializing random seed.
-  initializeRandomSeed(); 
+    // Initializing random seed.
+    initializeRandomSeed();
 }
-
 
 // Helper to make playing sounds from the V5 in VEXcode easier and
 // keeps the code cleaner by making it clear what is happening.
-void playVexcodeSound(const char *soundName) {
-  printf("VEXPlaySound:%s\n", soundName);
-  wait(5, msec);
+void playVexcodeSound(const char* soundName) {
+    printf("VEXPlaySound:%s\n", soundName);
+    wait(5, msec);
 }
-
-
 
 // define variable for remote controller enable/disable
 bool RemoteControlCodeEnabled = true;
@@ -80,39 +71,181 @@ bool RemoteControlCodeEnabled = true;
 #pragma endregion VEXcode Generated Robot Configuration
 
 // ----------------------------------------------------------------------------
-//                                                                            
-//    Project:        Split Arcade Control                                         
-//    Description:    This example will use the left Y and right X
-//                    Controller axis to control the Clawbot.
-//    Configuration:  V5 Clawbot (Individual Motors)
-//                    Controller
-//                    Claw Motor in Port 3
-//                    Arm Motor in Port 8
-//                    Left Motor in Port 1
-//                    Right Motor in Port 10   
-//                                                                            
+//    Project:        Clawbot for Intro to Robotics (Period 5; Group 1)
 // ----------------------------------------------------------------------------
 
-// Include the V5 Library
 #include "vex.h"
 
-// Allows for easier use of the VEX Library
 using namespace vex;
 
-int main() {
-  // Initializing Robot Configuration. DO NOT REMOVE!
-  vexcodeInit();
-  // Begin project code.
-  // Main Controller loop to set motors to controller axis postiions
-  while (true) {
-    LeftMotor.setVelocity((Controller1.Axis3.position() + Controller1.Axis1.position()), percent);
-    RightMotor.setVelocity((Controller1.Axis3.position() - Controller1.Axis1.position()), percent);
-    LeftMotor.spin(forward);
-    RightMotor.spin(forward);
+bool autoMode = false;
+bool arcade = false;
 
-    wait(5, msec);
-  }
+// Begin project code
 
-  return 0;
+void controller_L1_Pressed() {
+    ArmMotor.spin(forward);
+    while (Controller1.ButtonL1.pressing()) {
+        wait(5, msec);
+    }
+    ArmMotor.stop();
 }
 
+void controller_L2_Pressed() {
+    ArmMotor.spin(reverse);
+    while (Controller1.ButtonL2.pressing()) {
+        wait(5, msec);
+    }
+    ArmMotor.stop();
+}
+
+void controller_R1_Pressed() {
+    ClawMotor.spin(reverse);
+    while (Controller1.ButtonR1.pressing()) {
+        wait(5, msec);
+    }
+    ClawMotor.stop();
+}
+
+void controller_R2_Pressed() {
+    ClawMotor.spin(forward);
+    while (Controller1.ButtonR2.pressing()) {
+        wait(5, msec);
+    }
+    ClawMotor.stop();
+}
+
+void controller_ButtonA_Pressed() { autoMode = !autoMode; }
+
+void controller_ButtonB_Pressed() { arcade = !arcade; }
+
+/**
+ * @param target - target angle in degrees
+ * @param direction - forward or reverse arm direction
+ * !!! @todo after passing the target angle, automatically determine if the
+ *       motor has to go in the forward or reverse direction to reach
+ *       the target angle
+ */
+void raiseArmPID(double target, vex::directionType direction) {
+    double kP = 0.5;
+    double kI = 0.01;
+    double kD = 0.1;
+
+    double error;
+    double prevErr = 0;
+    double integral = 0;
+    double derivative;
+
+    while (true) {
+        double currAngle = Potentiometer.angle(degrees);
+        error = target - currAngle;
+        derivative = error - prevErr;
+
+        double motorPower = (error * kP) + (integral * kI) + (derivative * kD);
+        ArmMotor.spin(direction, motorPower, voltageUnits::volt);
+
+        prevErr = error;
+        if (fabs(error) < 1.0) {
+            break;
+        }
+
+        wait(20, msec);
+    }
+
+    ArmMotor.stop();
+}
+
+int main() {
+    // Initializing Robot Configuration. DO NOT REMOVE!
+    vexcodeInit();
+
+    // Create Controller callback events
+    Controller1.ButtonL1.pressed(controller_L1_Pressed);
+    Controller1.ButtonL2.pressed(controller_L2_Pressed);
+    Controller1.ButtonR1.pressed(controller_R1_Pressed);
+    Controller1.ButtonR2.pressed(controller_R2_Pressed);
+    Controller1.ButtonA.pressed(controller_ButtonA_Pressed);
+    Controller1.ButtonB.pressed(controller_ButtonB_Pressed);
+    wait(15, msec);
+
+    // Configure Arm and Claw motor hold settings and velocity
+    ArmMotor.setStopping(hold);
+    ClawMotor.setStopping(hold);
+    ArmMotor.setVelocity(50, percent);
+    ClawMotor.setVelocity(60, percent);
+
+    bool triggered = false;
+    bool armRasied = false;
+
+    // Main Controller loop to set motors to controller axis postiions
+    while (true) {
+        if (arcade) {
+            LeftMotor.setVelocity(
+                (Controller1.Axis2.position() + Controller1.Axis4.position()),
+                percent);
+            RightMotor.setVelocity(
+                (Controller1.Axis2.position() - Controller1.Axis4.position()),
+                percent);
+        } else {
+            LeftMotor.setVelocity(Controller1.Axis3.position(), percent);
+            RightMotor.setVelocity(Controller1.Axis2.position(), percent);
+        }
+
+        LeftMotor.spin(forward);
+        RightMotor.spin(forward);
+
+        Brain.Screen.clearScreen();
+        Brain.Screen.setCursor(1, 1);
+
+        Brain.Screen.print("Found Object?: %s",
+                           RangeFinderA.foundObject() ? "TRUE" : "FALSE");
+        Brain.Screen.newLine();
+
+        Brain.Screen.print("Distance - Inches: %.2f",
+                           RangeFinderA.distance(inches));
+        Brain.Screen.newLine();
+
+        Brain.Screen.print("Pot Angle: %.2f", Potentiometer.angle(degrees));
+        Brain.Screen.newLine();
+
+        Brain.Screen.print("Arm Motor Angle: %.2f", ArmMotor.position(degrees));
+        Brain.Screen.newLine();
+        wait(5, msec);
+
+        /**
+         * IF: object within three inches of robot's rear, automatically move
+         * forward 3 inches & raise arm
+         *
+         * Max Pot: 57.92
+         * Min: 0.24
+         */
+
+        if (autoMode && RangeFinderA.foundObject() &&
+            RangeFinderA.distance(inches) < 3 && !triggered) {
+            triggered = true;
+            // LeftMotor.setVelocity(100, percent);
+            // RightMotor.setVelocity(100, percent);
+            // LeftMotor.spinFor(forward, 360, degrees, false);
+            // RightMotor.spinFor(forward, 360, degrees, false);
+            // waitUntil(!LeftMotor.isSpinning() && !RightMotor.isSpinning());
+
+            // ArmMotor.setVelocity(100, percent);
+            // ClawMotor.setVelocity(100, percent);
+
+            // while (Potentiometer.angle(degrees) < 46) {
+            //     ArmMotor.spin(forward);
+            //     wait(10, msec);
+            // }
+
+            raiseArmPID(45, forward);
+
+            // ArmMotor.stop();
+        }
+
+        if (autoMode && RangeFinderA.distance(inches) >= 3) {
+            triggered = false;
+        }
+
+        wait(20, msec);
+    }
+}
